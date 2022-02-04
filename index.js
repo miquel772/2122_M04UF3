dd5f8b8ece32e7f9129e9e14c7b24f3b6210a7a9
enti <enti@enti.enti>
2022-02-04 14:22:00 +0100

    Classe 04/02

---
+++ index.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+
+
+let http = require("http");
+let fs = require("fs");
+
+let http_server = http.createServer(function(req, res){
+
+	if(req.url == "/character.png"){
+		
+		fs.readFile("character.png", function(err, data){
+			if(err){ console.log("ERROR!"); return; }
+	
+		res.writeHead(200);
+		res.end(data);
+		});
+	}
+
+	fs.readFile("index.html", function(err, data){
+		if(err){ console.log("ERROR!"); return; }
+
+	res.writeHead(200);
+
+	res.end(data);
+
+	});
+
+}).listen(1095);
