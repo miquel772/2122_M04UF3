eeff1c705ef0417b821415734553ee0aabece9eb
enti <enti@enti.enti>
2022-02-11 14:26:59 +0100

    cruzar la calle 11/02

--- index.js
+++ index.js
@@ -6,17 +6,12 @@ let fs = require("fs");
 
 let http_server = http.createServer(function(req, res){
 
-	if(req.url == "/character.png"){
-		
-		fs.readFile("character.png", function(err, data){
-			if(err){ console.log("ERROR!"); return; }
-	
-		res.writeHead(200);
-		res.end(data);
-		});
+	var archivo = "index.html";
+	if (req.url != "/"){
+		archivo = req.url.substring(1);
 	}
 
-	fs.readFile("index.html", function(err, data){
+	fs.readFile(archivo, function(err, data){
 		if(err){ console.log("ERROR!"); return; }
 
 	res.writeHead(200);
